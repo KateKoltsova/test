644e5ec7aea46e8ca241b85fc32c1a9f7a117427
Kate Koltsova <kate.petronyuk@gmail.com>
2022-04-19 13:19:17 +0300

    Update index.php in master branch

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $text = 'Hello, World!';
+$message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 ?>
 
 <!DOCTYPE html>
