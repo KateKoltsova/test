47022d32f507db69f81d4ea619fc2b3958f68af9
Kate Koltsova <kate.petronyuk@gmail.com>
2022-04-19 13:09:54 +0300

    Added html" "

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
 $text = 'Hello, World!';
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+    <h1><?php echo $text ?></h1>
+
+</body>
+</html>
